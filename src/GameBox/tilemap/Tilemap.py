8e164abb326dbde83afc75ba4b3617184362f2d9
Sam Fertig <sfertig007@gmail.com>
2026-02-03 18:02:50 -0500

    startd tilemap dict change

--- src/GameBox/tilemap/Tilemap.py
+++ src/GameBox/tilemap/Tilemap.py
@@ -5,7 +5,7 @@ from ..basics.Net import Global
 from ..basics.ui import load_image
 
 class Tilemap:
-    def __init__(self, tilesetImage, tileDim, scale, mapDim, fill=0, layer=4, show=True):
+    def __init__(self, tilesetImage, tileDim, scale, layer=4, show=True):
         self.tilemap = load_image(tilesetImage)
 
         self.tileDim = pygame.Vector2(tileDim)
@@ -19,7 +19,7 @@ class Tilemap:
         Global.objs[str(self.layer)].append(self)
 
         #load images and create basic tilemap 
-        self.map = np.full(mapDim, fill)
+        self.map = {}
         self.tiles: Dict[int, pygame.Surface] = {}
         self.images: Dict[int, pygame.Surface] = {}
         tile = 1
@@ -37,14 +37,10 @@ class Tilemap:
         if self.show: self.draw()
 
     def draw(self):
-        for y in range(self.mapDim[1]):
-            for x in range(self.mapDim[0]):
-                if self.map[y][x] == 0:
-                    continue
-                tile = self.tiles[self.map[y][x]]
-                mx = ((x * self.scaleDim.x) - Global.cam.pos.x)
-                my = ((y * self.scaleDim.y) - Global.cam.pos.y)
-                if mx < -self.scaleDim.x or mx > Global.screenDim.x or my < -self.scaleDim.y or my > Global.screenDim.y: continue
-                Global.screen.blit(tile, (mx, my))
+        for key in self.map:
+            tile = self.map[key]
+            image = self.tiles[tile['type']]
+            pos = tile['pos']
+            
             
 
