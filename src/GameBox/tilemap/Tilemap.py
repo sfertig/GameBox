94d04d0ca3652cbe4da8a066901d8b635a5b1f3f
Sam Fertig <sfertig007@gmail.com>
2026-02-16 20:41:33 -0500

    added dict and json tilemap loading, next is editor basics

--- src/GameBox/tilemap/Tilemap.py
+++ src/GameBox/tilemap/Tilemap.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import json
 
 from ..basics.Net import Global
 from ..basics.ui import load_image
@@ -11,27 +12,39 @@ class Tilemap:
         self.tileDim = pygame.Vector2(tileDim)
         self.scaleDim = pygame.Vector2(tileDim) * scale
         self.scale = scale
-        self.mapDim = mapDim
 
-        self.fill = fill
         self.layer = layer
         self.show = show
         Global.objs[str(self.layer)].append(self)
 
         #load images and create basic tilemap 
         self.map = {}
+        self.mapPath = None
         self.tiles: Dict[int, pygame.Surface] = {}
         self.images: Dict[int, pygame.Surface] = {}
         tile = 1
         #split image into tiles
-        for y in range(mapDim[1]):
-            for x in range(mapDim[0]):
+        for y in range(int(self.tilemap.get_height() // self.tileDim.y)):
+            for x in range(int(self.tilemap.get_width() // self.tileDim.x)):
                 try:
                     self.images[tile] = self.tilemap.subsurface(pygame.Rect(x * self.tileDim.x, y * self.tileDim.y, self.tileDim.x, self.tileDim.y))
                     self.tiles[tile] = pygame.transform.scale_by(self.images[tile], self.scale)
                 except:
                     pass
                 tile += 1
+
+    def load_from_dict(self, data):
+        self._private_load(None, data)
+
+    def load_from_json(self, path):
+        with open(path, 'r') as f:
+            data = json.load(f)
+        self._private_load(path, data)
+
+    #private load func
+    def _private_load(self, Path, data):
+        self.map = data
+        self.mapPath = Path
 
     def update(self):
         if self.show: self.draw()
@@ -41,6 +54,10 @@ class Tilemap:
             tile = self.map[key]
             image = self.tiles[tile['type']]
             pos = tile['pos']
+            x = (pos[0] * self.scaleDim.x) + Global.cam.pos.x
+            y = (pos[1] * self.scaleDim.y) + Global.cam.pos.y
+            if x < Global.cam.pos.x - Global.screenDim.x or x > Global.cam.pos.x + Global.screenDim.x or y < Global.cam.pos.y - Global.screenDim.y or y > Global.cam.pos.y + Global.screenDim.y: continue
+            Global.screen.blit(image, (x, y))
             
             
 
--- tests/GettingStarted.py
+++ tests/GettingStarted.py
@@ -10,26 +10,25 @@ game = Game(width, height, resizable=True)
 screen = game.get_screen()
 
 cam = Cammera()
-cam.pos += pygame.Vector2(width/2, height/2)
 
-player = Player(pygame.Vector2(width, height), pygame.Vector2(50, 50), "blue")
+player = Player(pygame.Vector2(width/2, height/2), pygame.Vector2(50, 50), "blue")
 player.add_physics(0, 0, 0, pygame.Vector2(100, 100), pygame.Vector2(0.5, 0.5))
 
 
 score = 0
 scoreUI = Text(pygame.Vector2(0, 0), 'player1: 0', pygame.font.SysFont("Arial", 32), "white")
 
-game.generate_bounds(width*2, height*2, "blue", True)
+game.generate_bounds(width, height)
+
 
 orbs = []
 #fill in list
-for _ in range(25) : # this will be the number of orbs
-	pos = pygame.Vector2(Ri(0, width), Ri(0, height))
-	size = Ri(25, 50)
-	color = (Ri(0, 255), Ri(0, 255), Ri(0, 255))
-	orbs.append(Rect(pos, pygame.Vector2(size, size), color, collision=False))
+for _ in range(75) : # this will be the number of orbs
+    size = Ri(25, 50)
+    pos = pygame.Vector2(Ri(0+size, width-size), Ri(0+size, height-size))
+    color = (Ri(0, 255), Ri(0, 255), Ri(0, 255))
+    orbs.append(Rect(pos, pygame.Vector2(size, size), color, collision=False))
 
-cam.set_target(player)
 
 running = True
 while running:
@@ -44,7 +43,7 @@ while running:
             orb.delete()
             orbs.remove(orb)
             score += 1
-            scoreUI.change(f"player1: {score}")
+            scoreUI.change(f"player: {score}")
             cam.shake(3, 2, (0, 0))
 
     player.move.by_WSAD(7.25) 
